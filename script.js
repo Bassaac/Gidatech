c8edd9578ffccc0001d6f31f8394525f13fff9c5
Bassaac <robertsisaacrobo@gmail.com>
2023-02-21 10:42:01 -0800

    new commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('.icon');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+function toggle() {
+    const content = document.querySelector('.content')
+    content.classList.toggle('active');
+}
+
+
