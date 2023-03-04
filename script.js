6dc497e515a247521d369d768d1dc1aa1adee8c8
Bassaac <robertsisaacrobo@gmail.com>
2023-03-04 02:39:15 -0800

    configured mailing

--- script.js
+++ script.js
@@ -19,4 +19,36 @@ function toggle2() {
     vidover.classList.toggle('active2')
     console.log('true')
 }
+// document.getElementById("formm").addEventListener("click", function (event) {
+//     event.preventDefault(); // Prevent the form from submitting normally
+//     var form = document.getElementById("submit-button"); // Replace "your-form-id" with the actual ID of your form
+//     var formData = new FormData(form);
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", form.action, true);
+//     xhr.setRequestHeader("Accept", "application/json");
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//         if (xhr.status === 200) {
+//             window.location.href = "https://gidatech.ng"; // Replace "https://your-redirect-url.com" with the actual URL of the page you want to redirect the user to
+//         } else {
+//             console.error(xhr.response);
+//         }
+//     };
+//     xhr.send(formData);
+// });
+
+
+// document.getElementById("submit-button").addEventListener("click", function (event) {
+//     event.preventDefault(); // Prevent the form from submitting normally
+//     alert("Thank you for submitting the form. We'll get back to you");
+//     document.getElementById("formm").submit(); // Submit the form normally
+// });
+document.getElementById("submit-button").addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+    alert("Thank you for submitting the form. We'll get back to you");
+    var form = document.getElementById("formm"); // Replace "your-form-id" with the actual ID of your form
+    form.submit(); // Submit the form
+    window.location.href = "index.html"; // Redirect the user to your index.html page
+});
+
 
